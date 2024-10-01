c18fdc2784023f36546b32137d20d08701f5d9a2
lorenzo-de-giorgi <lorenzodegiorgi2004@gmail.com>
2024-10-01 17:29:16 +0200

    added CORS Policy to get API calls from front-end. added "/login" and "/register" to thest the api from front-end

--- app.rb
+++ app.rb
@@ -3,3 +3,23 @@ require "sequel"
 require "bcrypt"
 require "json"
 require "rack/cors"
+
+# CORS Policy per autorizzare il front-end ad effettuare chiamate verso questo endpoint(http://localhost:4567)
+use Rack::Cors do
+  allow do
+    origins 'http://localhost:3000'
+    resource '*',
+      headers: :any,
+      methods: [:get, :post, :put, :patch, :delete, :options, :head],
+      max_age: 600
+  end
+end
+
+
+post '/login' do
+  "Login Button"
+end
+
+post '/register' do
+  "Register Button"
+end
