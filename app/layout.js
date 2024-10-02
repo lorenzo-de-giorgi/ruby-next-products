3ab0238c5645ba6947f5efa406dd9fd25ab7a5a7
lorenzo-de-giorgi <lorenzodegiorgi2004@gmail.com>
2024-10-02 11:53:45 +0200

    modified layout.js page in register page added the button to submit and added a console.log to test the input value

--- app/layout.js
+++ app/layout.js
@@ -3,35 +3,14 @@
 import './globals.css';
 import Link from 'next/link';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import axios from 'axios';
 
 export default function RootLayout({ children }){
-  const testLogin = async (e) => {
-    e.preventDefault();
-    try {
-      const response = await axios.post('http://localhost:4567/login');
-      console.log(response.data);
-    } catch (error) {
-      console.error("Errore nella richiesta:", error);
-    }
-  }
-
-  const testRegister = async (e) => {
-    e.preventDefault();
-    try {
-      const response = await axios.post('http://localhost:4567/register');
-      console.log(response.data);
-    } catch (error) {
-      console.error("Errore nella richiesta:", error);
-    }
-  }
-  
   return(
     <html>
       <body className='container'>
         <nav className='d-flex justify-content-end mt-3'>
-          <Link onClick={testLogin} href="/login" className='me-3 btn btn-dark'>Accedi</Link>
-          <Link onClick={testRegister} href="/register" className='btn btn-close-white'>Registrati</Link>
+          <Link href="/login" className='me-3 btn btn-dark'>Accedi</Link>
+          <Link href="/register" className='btn btn-close-white'>Registrati</Link>
         </nav>
         <main>
           {children}
--- app/register/page.js
+++ app/register/page.js
@@ -1,17 +1,36 @@
+"use client"
+
 import Link from "next/link";
+import { useState } from "react";
+import { useRouter } from 'next/navigation';
 
 export default function RegisterPage() {
+    const [username, setUsername] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
+    const [name, setName] = useState('');
+    const [surname, setSurname] = useState('');
+    const [date_of_birth, setBirthday] = useState('');
+    const router = useRouter();
+
+    const handleRegister = (e) => {
+        e.preventDefault();
+        // console.log per verificare il corretto recupero dei dati
+        console.log(username, email, password, name, surname, date_of_birth);
+    }
     return(
         <div className="container">
             <h1 className="text-center">Register</h1>
-            <form className="d-flex flex-column">
-                <input type="email" placeholder="Email" required className="form-control p-2 mb-3 mt-3"></input>
-                <input type="text" placeholder="Username" required className="form-control p-2 mb-3"></input>
-                <input type="password" placeholder="Password" required className="form-control p-2 mb-3"></input>
-                <input type="password" placeholder="Confirm Password" required className="form-control p-2 mb-3"></input>
-                <input type="text" placeholder="Name" required className="form-control p-2 mb-3"></input>
-                <input type="text" placeholder="Surname" required className="form-control p-2 mb-3"></input>
-                <input type="date" placeholder="Date Of Birth" required className="form-control p-2 mb-3"></input>
+            <form className="d-flex flex-column" onSubmit={handleRegister}>
+                <input value={email} onChange={(e) => setEmail(e.target.value)} type="email" placeholder="Email" required className="form-control p-2 mb-3 mt-3"></input>
+                <input value={username} onChange={(e) => setUsername(e.target.value)}  type="text" placeholder="Username" required className="form-control p-2 mb-3"></input>
+                <input value={password} onChange={(e) => setPassword(e.target.value)} type="password" placeholder="Password" required className="form-control p-2 mb-3"></input>
+                <input value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)} type="password" placeholder="Confirm Password" required className="form-control p-2 mb-3"></input>
+                <input value={name} onChange={(e) => setName(e.target.value)} type="text" placeholder="Name" required className="form-control p-2 mb-3"></input>
+                <input value={surname} onChange={(e) => setSurname(e.target.value)} type="text" placeholder="Surname" required className="form-control p-2 mb-3"></input>
+                <input value={date_of_birth} onChange={(e) => setBirthday(e.target.value)} type="date" placeholder="Date Of Birth" required className="form-control p-2 mb-3"></input>
+                <button type="submit">Registrati</button>
             </form>
             <p className="text-center mt-4">Sei già registrato? <Link href="/login">Accedi</Link></p>
         </div>
