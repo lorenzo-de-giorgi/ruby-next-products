19779a38e734845f4c771a675603f642e12b1b47
lorenzo-de-giorgi <lorenzodegiorgi2004@gmail.com>
2024-10-02 11:01:24 +0200

    created tables (users, product_types, products) for DB

--- app.rb
+++ app.rb
@@ -19,10 +19,37 @@ end
 DB = Sequel.connect(
   adapter: 'postgres',
   host: 'localhost',
-  database: 'products_db'
+  database: 'products_db',
   user: 'postgres',
   password: '15122004'
 )
+
+# Controllo l'esistenza e creo la tabella "users"
+DB.create_table?(:users) do
+  primary_key :id
+  String :username, null: false, unique: true, size: 75
+  String :email, null: false, unique: true, size: 100
+  String :password_hash, null: false, unique: false, size: 25
+  String :name, null: false, size: 20
+  String :surname, null: false, size: 30
+  Date :date_of_birth, null: false
+  DateTime :created_at, default: Sequel::CURRENT_TIMESTAMP
+end
+
+# Controllo l'esistenza e creo la tabella "product_types"
+DB.create_table?(:product_types) do
+  primary_key :id
+  String :type, null: false, size: 40
+  DateTime :created_at, default: Sequel::CURRENT_TIMESTAMP
+end
+
+# Controllo l'esistenza e creo la tabella "products"
+DB.create_table?(:products) do
+  primary_key :id
+  String :name, null: false, size: 50
+  String :description, null: false, size: 255
+  DateTime :created_at, default: Sequel::CURRENT_TIMESTAMP
+end
 
 post '/login' do
   "Login Button"
