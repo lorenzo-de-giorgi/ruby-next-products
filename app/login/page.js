64c6237082c7d1a9e025f59b27635e8a2d078261
lorenzo-de-giorgi <lorenzodegiorgi2004@gmail.com>
2024-10-03 19:15:57 +0200

    modified login system

--- app/login/page.js
+++ app/login/page.js
@@ -1,9 +1,10 @@
 "use client";
 
-import axios from "axios";
 import Link from "next/link";
 import { useState } from "react";
 import { Button } from "react-bootstrap";
+import { signIn } from 'next-auth/react';
+import { useRouter } from 'next/router';
 
 export default function LoginPage() {
     const [username, setUsername] = useState('');
@@ -11,46 +12,30 @@ export default function LoginPage() {
 
     const handleLogin = async (e) => {
         e.preventDefault();
-        // console.log(username, password);
-        const formData = {
-            username,
-            password
-        }
 
-        try{
-            const response = await axios.post('http://localhost:4567/login', formData, {
-              headers: {
-                'Content-Type': 'application/json',
-              }
-            })
-      
-            if (response.status === 200) {
-              alert('Registration successful!');
-              // Redirigi alla pagina di login
-              router.push('/login');
-            } else {
-              alert(`Login avvenuto con successo`);
-            }
-            
-          } catch(error) {
-            if (error.response) {
-              // Il server ha restituito una risposta con un codice di stato diverso da 2xx
-              alert(`Login fallito: ${error.response.data.message}`);
-            } else {
-              // Errore nella richiesta
-              alert('An error occurred during login. Please try again later.');
-            }
-          }
+        // Utilizza signIn di next-auth per autenticare l'utente con il provider "credentials"
+        const result = await signIn('credentials', {
+            redirect: false, // Impedisce il redirect automatico
+            username,
+            password,
+        });
+
+        if (result?.error) {
+            alert(`Login fallito: ${result.error}`);
+        } else {
+            alert('Login avvenuto con successo!');
+        }
     }
-    return(
+
+    return (
         <div className="container">
             <h1 className="text-center">Login</h1>
             <form onSubmit={handleLogin} className="d-flex flex-column">
-                <input value={username} onChange={(e) => setUsername(e.target.value)} type="text" placeholder="Username" required className="form-control p-2 mb-3 mt-3"></input>
-                <input value={password} onChange={(e) => setPassword(e.target.value)} type="password" placeholder="Password" required className="form-control p-2"></input>
+                <input value={username} onChange={(e) => setUsername(e.target.value)} type="text" placeholder="Username" required className="form-control p-2 mb-3 mt-3"/>
+                <input value={password} onChange={(e) => setPassword(e.target.value)} type="password" placeholder="Password" required className="form-control p-2"/>
                 <Button type="submit" className="mt-3">Login</Button>
             </form>
             <p className="text-center mt-4">Non sei ancora registrato? <Link href="/register">Registrati</Link></p>
         </div>
-    )
+    );
 }
