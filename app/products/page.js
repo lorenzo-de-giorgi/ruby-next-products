726612021b3aaaea64c6c0c94ec19e5b96a7a8e8
lorenzo-de-giorgi <lorenzodegiorgi2004@gmail.com>
2024-10-08 16:48:04 +0200

    fixed some errors in code

--- app/products/page.js
+++ app/products/page.js
@@ -1,7 +1,6 @@
 "use client";
 
-import { useState } from "react";
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 import { useRouter } from "next/navigation";
 import { useSession } from "next-auth/react";
 import { Button, Offcanvas, FormSelect, Modal } from "react-bootstrap";
@@ -24,36 +23,42 @@ export default function Inventory() {
     const router = useRouter();
     const { data: session, status } = useSession();
 
-    const [show, setShow] = useState(false); // Stato per gestire la visibilità dell'Offcanvas
-    const handleClose = () => setShow(false); // Funzione per chiudere l'Offcanvas
-    const handleShow = ( product = null ) => {
-        if(product){
-            setName(product.name);
-            setDescription(product.description);
-            setSelectedType(product.category);
-            setSelectedProduct(product);
-        } else {
-            setName('');
-            setDescription('');
-            setSelectedType('');
-            setSelectedProduct('');
-        }
-        setShow(true);
-    }
+    // Stati per gestire la visibilità dei diversi Offcanvas
+    const [showCreationOffCanvas, setShowCreationOffCanvas] = useState(false);
+    const [showUpdateOffCanvas, setShowUpdateOffCanvas] = useState(false);
 
-    const handleDetailClose = () => setShowDetail(false); // Close detail modal
+    // Funzioni per chiudere gli Offcanvas
+    const handleCloseCreationOffCanvas = () => setShowCreationOffCanvas(false);
+    const handleCloseUpdateOffCanvas = () => setShowUpdateOffCanvas(false);
+
+    // Funzioni per mostrare gli Offcanvas
+    const handleShowCreationOffCanvas = () => {
+        setName('');
+        setDescription('');
+        setSelectedType('');
+        setSelectedProduct(null);
+        setShowCreationOffCanvas(true);
+    };
+
+    const handleShowUpdateOffCanvas = (product) => {
+        setName(product.name);
+        setDescription(product.description);
+        setSelectedType(product.category);
+        setSelectedProduct(product);
+        setShowUpdateOffCanvas(true);
+    };
+
+    const handleDetailClose = () => setShowDetail(false);
     const handleShowDetail = (product) => {
         setSelectedProduct(product);
         setShowDetail(true);
     };
 
-    const handleSelectChange = (e) => {
-        setSelectedType(e.target.value);
-    };
+    const handleSelectChange = (e) => setSelectedType(e.target.value);
 
     useEffect(() => {
-        if(status === "loading") return;
-        if(!session) router.push('/login');
+        if (status === "loading") return;
+        if (!session) router.push('/login');
 
         const fetchProductTypes = async () => {
             try {
@@ -74,8 +79,7 @@ export default function Inventory() {
         };
 
         fetchProductTypes();
-        fetchProducts(); 
-
+        fetchProducts();
     }, [session, status, router]);
 
     const handleProductCreation = async (e) => {
@@ -86,54 +90,35 @@ export default function Inventory() {
             return;
         }
 
-        if (!name || !description || !selectedType) {
-            alert("Compila tutti i campi obbligatori");
-            return;
-        }
-
         try {
-            // Ottienengo user_id dalla sessione
             const userId = session.user.id;
 
             const productData = {
-            user_id: userId,
-            product_type_id: selectedType,
-            name: name,
-            description: description
+                user_id: userId,
+                product_type_id: selectedType,
+                name: name,
+                description: description
             };
 
-            // Invio della richiesta al server
             const response = await axios.post('http://localhost:4567/create_product', productData, {
-                headers: {
-                    'Content-Type': 'application/json',
-                },
+                headers: { 'Content-Type': 'application/json' },
             });
 
-            // Gestisci la risposta del server
             if (response.status === 201) {
-            alert('Prodotto creato con successo');
-            handleClose(); // Chiudi l'Offcanvas dopo il successo
-            router.reload(); // Ricarica la pagina o aggiorna la lista dei prodotti
+                alert('Prodotto creato con successo');
+                handleCloseCreationOffCanvas();
+                router.reload();
             } else {
-            alert(`Errore: ${response.data.error}`);
+                alert(`Errore: ${response.data.error}`);
             }
         } catch (error) {
             console.error("Errore durante la creazione del prodotto", error);
             alert("Errore durante la creazione del prodotto");
         }
-    }
+    };
 
     const handleProductUpdate = async (e) => {
         e.preventDefault();
-
-        const fetchProducts = async () => {
-            try {
-                const response = await axios.get('http://localhost:4567/products');
-                setProducts(response.data);
-            } catch (err) {
-                setError(err.message);
-            }
-        };
 
         if (!name || !description || !selectedType) {
             alert("Compila tutti i campi obbligatori");
@@ -151,15 +136,13 @@ export default function Inventory() {
             };
 
             const response = await axios.put(`http://localhost:4567/update_product/${selectedProduct.id}`, productData, {
-                headers: {
-                    'Content-Type': 'application/json',
-                },
+                headers: { 'Content-Type': 'application/json' },
             });
 
             if (response.status === 200) {
                 alert('Prodotto aggiornato con successo');
-                handleClose();
-                fetchProducts();
+                handleCloseUpdateOffCanvas();
+                setProducts(products.map(p => (p.id === selectedProduct.id ? response.data : p)));
             } else {
                 alert(`Errore: ${response.data.error}`);
             }
@@ -167,16 +150,14 @@ export default function Inventory() {
             console.error("Errore durante l'aggiornamento del prodotto", error);
             alert("Errore durante l'aggiornamento del prodotto");
         }
-
-    }
+    };
 
     const handleProductDelete = async (productId) => {
-        if(window.confirm("Sei sicuro di voler cancellare questo prodotto?")){
+        if (window.confirm("Sei sicuro di voler cancellare questo prodotto?")) {
             try {
-                const response = await axios.delete(`http://localhos:4567/delete_product/${productId}`);
+                const response = await axios.delete(`http://localhost:4567/delete_product/${productId}`);
                 if (response.status === 204) {
                     alert('Prodotto eliminato con successo');
-                    // Ricarica i prodotti o aggiorna lo stato
                     setProducts(products.filter(product => product.id !== productId));
                 } else {
                     alert(`Errore: ${response.data.error}`);
@@ -186,27 +167,24 @@ export default function Inventory() {
                 alert("Errore durante l'eliminazione del prodotto");
             }
         }
-    }
+    };
 
-    if (status === "loading") {
-        return <div>Caricamento...</div>
-    }
+    if (status === "loading") return <div>Caricamento...</div>;
 
-    if (!session) {
-        return null; // This will prevent the page content from flashing before redirect
-    }
-    
-    return(
+    if (!session) return null; // Prevent content flash before redirect
+
+    return (
         <div className="container">
             <h1 className="text-center">Inventario</h1>
             <div className="d-flex justify-content-end me-5">
-                <Button onClick={handleShow}><FontAwesomeIcon icon={faPlus} /></Button>
-                <Offcanvas show={show} onHide={handleClose} placement="end">
+                <Button onClick={handleShowCreationOffCanvas}><FontAwesomeIcon icon={faPlus} /></Button>
+                {/* Offcanvas for Product Creation */}
+                <Offcanvas show={showCreationOffCanvas} onHide={handleCloseCreationOffCanvas} placement="end">
                     <Offcanvas.Header closeButton>
-                        <Offcanvas.Title>{selectedProduct ? "Modifica Prodotto" : "Aggiungi Prodotto"}</Offcanvas.Title>
+                        <Offcanvas.Title>Aggiungi Prodotto</Offcanvas.Title>
                     </Offcanvas.Header>
                     <Offcanvas.Body>
-                        <form>
+                        <form onSubmit={handleProductCreation}>
                             <div className="mb-3">
                                 <label htmlFor="productName" className="form-label">Nome Prodotto *</label>
                                 <input value={name} onChange={(e) => setName(e.target.value)} type="text" className="form-control" id="productName" />
@@ -217,7 +195,7 @@ export default function Inventory() {
                             </div>
                             <div className="mb-3">
                                 <label htmlFor="productCategory" className="form-label">Seleziona Tipo *</label>
-                                <br></br>
+                                <br />
                                 <FormSelect id="productTypeSelect" value={selectedType} onChange={handleSelectChange}>
                                     <option value="">Scegli un tipo</option>
                                     {productTypes.map((type) => (
@@ -226,19 +204,49 @@ export default function Inventory() {
                                 </FormSelect>
                                 {selectedType && <div>Tipo di prodotto selezionato: {selectedType}</div>}
                             </div>
-                            <Button onClick={selectedProduct ? handleProductUpdate : handleProductCreation} variant="primary" type="submit">
-                                {selectedProduct ? "Salva" : "Aggiungi"}
-                            </Button>
+                            <Button variant="primary" type="submit">Crea</Button>
+                        </form>
+                    </Offcanvas.Body>
+                </Offcanvas>
+
+                {/* Offcanvas for Product Update */}
+                <Offcanvas show={showUpdateOffCanvas} onHide={handleCloseUpdateOffCanvas} placement="end">
+                    <Offcanvas.Header closeButton>
+                        <Offcanvas.Title>Modifica Prodotto</Offcanvas.Title>
+                    </Offcanvas.Header>
+                    <Offcanvas.Body>
+                        <form onSubmit={handleProductUpdate}>
+                            <div className="mb-3">
+                                <label htmlFor="productName" className="form-label">Nome Prodotto *</label>
+                                <input value={name} onChange={(e) => setName(e.target.value)} type="text" className="form-control" id="productName" />
+                            </div>
+                            <div className="mb-3">
+                                <label htmlFor="productDescription" className="form-label">Descrizione *</label>
+                                <textarea value={description} onChange={(e) => setDescription(e.target.value)} className="form-control" id="productDescription" rows="3"></textarea>
+                            </div>
+                            <div className="mb-3">
+                                <label htmlFor="productCategory" className="form-label">Seleziona Tipo *</label>
+                                <br />
+                                <FormSelect id="productTypeSelect" value={selectedType} onChange={handleSelectChange}>
+                                    <option value="">Scegli un tipo</option>
+                                    {productTypes.map((type) => (
+                                        <option key={type.id} value={type.id}>{type.type}</option>
+                                    ))}
+                                </FormSelect>
+                                {selectedType && <div>Tipo di prodotto selezionato: {selectedType}</div>}
+                            </div>
+                            <Button variant="primary" type="submit">Aggiorna</Button>
                         </form>
                     </Offcanvas.Body>
                 </Offcanvas>
             </div>
+
             <table className="table table-striped mt-5">
                 <thead>
                     <tr>
                         <th scope="col">Id</th>
                         <th scope="col">Nome</th>
-                        <th scope="col">Description</th>
+                        <th scope="col">Descrizione</th>
                         <th scope="col">Categoria</th>
                         <th scope="col">Azioni</th>
                     </tr>
@@ -251,15 +259,15 @@ export default function Inventory() {
                             <td>{product.description}</td>
                             <td>{product.category}</td>
                             <td>
-                            {/* onClick={() => handleShowDetail(product)} */}
                                 <Link href={`/products/${product.id}`}><Button variant="secondary" className="me-2"><FontAwesomeIcon icon={faEye} /></Button></Link>
-                                <Button variant="warning" onClick={() => handleShow(product)} className="me-2"><FontAwesomeIcon icon={faPen} /></Button>
+                                <Button variant="warning" onClick={() => handleShowUpdateOffCanvas(product)} className="me-2"><FontAwesomeIcon icon={faPen} /></Button>
                                 <Button variant="danger" onClick={() => handleProductDelete(product.id)}><FontAwesomeIcon icon={faTrash} /></Button>
                             </td>
-                    </tr>
+                        </tr>
                     ))}
                 </tbody>
             </table>
+
             <Modal show={showDetail} onHide={handleDetailClose}>
                 <Modal.Header closeButton>
                     <Modal.Title>Dettagli Prodotto</Modal.Title>
@@ -274,11 +282,10 @@ export default function Inventory() {
                     )}
                 </Modal.Body>
                 <Modal.Footer>
-                    {/* <Button variant="warning" onClick={() => handleShow(selectedProduct)} className="me-2"><FontAwesomeIcon icon={faPen} /></Button> */}
                     <Button variant="danger" onClick={() => handleProductDelete(selectedProduct.id)}><FontAwesomeIcon icon={faTrash} /></Button>
                     <Button variant="secondary" onClick={handleDetailClose}>Chiudi</Button>
                 </Modal.Footer>
             </Modal>
         </div>
-    )
+    );
 }
